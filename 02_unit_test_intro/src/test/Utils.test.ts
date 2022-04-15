2699b6ef36e38b05a459bdfec3f9fe64b010f66b
SOunit <workbookwork7@gmail.com>
2022-04-14 17:39:09 -0700

    add sample

--- 02_unit_test_intro/src/test/Utils.test.ts
+++ 02_unit_test_intro/src/test/Utils.test.ts
@@ -2,7 +2,26 @@ import { Utils } from "../app/Utils";
 
 describe("Utils test suite", () => {
   test("first test", () => {
-    const abc = Utils.toUpperCase("abc");
-    console.log(abc);
+    const result = Utils.toUpperCase("abc");
+    expect(result).toBe("ABC");
+  });
+
+  test("parse simple URL", () => {
+    const parsedUrl = Utils.parseUrl("http://localhost:3050/login");
+    expect(parsedUrl.href).toBe("http://localhost:3050/login");
+    expect(parsedUrl.port).toBe("3050");
+    expect(parsedUrl.protocol).toBe("http:");
+    expect(parsedUrl.query).toEqual({});
+  });
+
+  test("parse url with query", () => {
+    const parsedUrl = Utils.parseUrl(
+      "http://localhost:3050/login?user=user&pass=pass"
+    );
+    const expectedQuery = {
+      user: "user",
+      pass: "pass",
+    };
+    expect(parsedUrl.query).toEqual(expectedQuery);
   });
 });
