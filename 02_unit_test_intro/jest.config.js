74a092f3cf9e1ca8856513467537e6a24798f926
SOunit <workbookwork7@gmail.com>
2022-04-14 17:20:35 -0700

    add jest setup for typescript

---
+++ 02_unit_test_intro/jest.config.js
@@ -0,0 +1,11 @@
+const { defaults } = require("jest-config");
+
+// copy and paste from official doc
+// https://jestjs.io/docs/configuration#roots-arraystring
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: { "\\.[jt]sx?$": "ts-jest" },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  verbose: true,
+};
